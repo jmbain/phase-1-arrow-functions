39887e4cbb1c3d62decb7d47e2dccf833f690f18
Josh Bain <joshuambain@gmail.com>
2024-07-17 22:26:42 -0400

    complete arrow functions lab

---
+++ index.js
@@ -0,0 +1,16 @@
+//notes from video lesson
+// function printBook(book) {
+//     console.log(book);
+//  }
+
+//  const printBookArrow = book => console.log(book)
+
+//  printBook('Neuromancer');
+//  printBookArrow('Burning Chrome')
+
+function divide() {
+   return 2000/100
+}
+ const square = num1 => num1*num1;
+
+ const add = (num1,num2) => (num1 + num2);
